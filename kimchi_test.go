7544b14d9eefff52e64c1172016006854e38f503
Masala <masala@riseup.net>
2019-01-09 06:03:09 -0500

    Remove signal handler

--- kimchi_test.go
+++ kimchi_test.go
@@ -1,12 +1,8 @@
 package kimchi
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
 	"testing"
 	"time"
-	//"github.com/stretchr/testify/assert"
 )
 
 func TestBootstrapNonvoting(t *testing.T) {
@@ -16,11 +12,8 @@ func TestBootstrapNonvoting(t *testing.T) {
 	nMix := 6
 	k := NewKimchi(basePort, "",  voting, nVoting, nProvider, nMix)
 	k.Run()
-
-	// Wait for a signal to tear it all down.
-	ch := make(chan os.Signal)
-	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
 	t.Logf("Running mixnet.")
+
 	go func() {
 		<-time.After(1 * time.Minute)
 		t.Logf("Received shutdown request.")
