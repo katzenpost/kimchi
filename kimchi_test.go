fe18be2f92e11bafc573789e87072855b0f3d6bd
Masala <masala@riseup.net>
2019-01-14 14:51:09 -0500

    Add threshold voting test

--- kimchi_test.go
+++ kimchi_test.go
@@ -5,11 +5,26 @@ import (
 	"testing"
 	"time"
 
+	aServer "github.com/katzenpost/authority/voting/server"
+	"github.com/katzenpost/core/crypto/cert"
 	"github.com/katzenpost/core/epochtime"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
+// Shutdown an authority
+func (k *kimchi) killAnAuth() bool {
+	for _, svr := range k.servers {
+		switch svr.(type) {
+		case *aServer.Server:
+			svr.Shutdown()
+			return true
+		}
+	}
+	return false
+}
+
 func TestBootstrapNonvoting(t *testing.T) {
+	assert := assert.New(t)
 	voting := false
 	nVoting := 0
 	nProvider := 2
@@ -19,9 +34,19 @@ func TestBootstrapNonvoting(t *testing.T) {
 	t.Logf("Running mixnet.")
 
 	go func() {
+		defer k.Shutdown()
 		<-time.After(1 * time.Minute)
 		t.Logf("Received shutdown request.")
-		k.Shutdown()
+		p, err := k.pkiClient()
+		if assert.NoError(err) {
+			epoch, _, _ := epochtime.Now()
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+			c, _, err := p.Get(ctx, epoch)
+			assert.NoError(err)
+			t.Logf("Got a consensus: %v", c)
+		}
+
 		t.Logf("All servers halted.")
 	}()
 
@@ -30,7 +55,7 @@ func TestBootstrapNonvoting(t *testing.T) {
 }
 
 func TestBootstrapVoting(t *testing.T) {
-	require := require.New(t)
+	assert := assert.New(t)
 	voting := true
 	nVoting := 3
 	nProvider := 2
@@ -40,18 +65,64 @@ func TestBootstrapVoting(t *testing.T) {
 	t.Logf("Running mixnet.")
 
 	go func() {
+		defer k.Shutdown()
 		<-time.After(3 * time.Minute)
+		t.Logf("Time is up!")
 		// verify that consensus was made
 		p, err := k.pkiClient()
-		require.NoError(err)
-		epoch, _, _ := epochtime.Now()
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-		defer cancel()
-		_, _, err = p.Get(ctx, epoch)
-		require.NoError(err)
-		t.Logf("Received shutdown request.")
-		k.Shutdown()
-		t.Logf("All servers halted.")
+		if assert.NoError(err) {
+			epoch, _, _ := epochtime.Now()
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+			c, _, err := p.Get(ctx, epoch)
+			assert.NoError(err)
+			t.Logf("Got a consensus: %v", c)
+		}
+	}()
+
+	k.Wait()
+	t.Logf("Terminated.")
+}
+
+func TestBootstrapVotingThreshold(t *testing.T) {
+	assert := assert.New(t)
+	voting := true
+	nVoting := 3
+	nProvider := 2
+	nMix := 6
+	k := NewKimchi(basePort, "",  voting, nVoting, nProvider, nMix)
+	k.Run()
+	t.Logf("Launching Mixnet")
+
+	go func() {
+		defer k.Shutdown()
+		// Varying this delay will set where in the
+		// voting protocl the authority fails.
+		<-time.After(30 * time.Second)
+		t.Logf("Killing an Authority")
+		if !assert.True(k.killAnAuth()) {
+			return
+		}
+		<-time.After(100 * time.Second)
+		t.Logf("Time is up!")
+		// verify that consensus was made
+		p, err := k.pkiClient()
+		if assert.NoError(err) {
+			epoch, _, _ := epochtime.Now()
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+			t.Logf("Fetching a consensus")
+			c, r, err := p.Get(ctx, epoch)
+			if assert.NoError(err) {
+				t.Logf("Got a consensus: %v", c)
+				s, err := cert.GetSignatures(r)
+				if assert.NoError(err) {
+					// Confirm exactly 2 signatures are present.
+					assert.Equal(2, len(s))
+					t.Logf("2 Signatures found on consensus as expected")
+				}
+			}
+		}
 	}()
 
 	k.Wait()
